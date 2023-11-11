7a8cf36d042993deec7cae4f509f9ebba2ff3a68
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-11 14:20:50 +0300

    add async await

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,10 @@ import emailjs from "@emailjs/browser";
   import "react-toastify/dist/ReactToastify.css";
 const Form = () => {
   const form = useRef();
-  const sendEmail = (e) => {
+  const sendEmail = async(e) => {
     e.preventDefault();
 
-    emailjs
+   await emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
