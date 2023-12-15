5e88c199deea867cfb9609724a3afd2f285e1e97
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-15 20:32:21 +0300

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,9 @@ import intoxica from "./intoxica.png";
 import mindease from "./mindease.png";
 import express from "./express.png";
 import shortener from "./url-shortener.png";
+import trendy from "./trendy.png";
+import calculator from "./calculator.png";
+import expense from "./expense.png";
 const projectsData = [
   {
     id: 1,
@@ -45,9 +48,36 @@ const projectsData = [
     id: 5,
     image: shortener,
     title: "Url Shortener",
-    description: "A url shortener built using React and Tinurl API",
+    description: "A url shortener built using React, TailwindCSS and Tinurl API",
     sourceCode: "https://github.com/geoffreykithuku/url-shortener",
     liveProject: "https://www.shorten-url.co",
+  },
+  {
+    id: 6,
+    image: trendy,
+    title: "Trendy",
+    description:
+      "A React + TailwindCSS shopping cart. It uses Redux Toolkit to manage state",
+    sourceCode: "https://github.com/geoffreykithuku/redux-toolkit-shop",
+    liveProject: "https://trendy-tau.vercel.app/",
+  },
+  {
+    id: 7,
+    image: calculator,
+    title: "Calculator",
+    description:
+      "A React + TailwindCSS calculator. It uses Context API to manage state",
+    sourceCode: "https://github.com/geoffreykithuku/react-calculator",
+    liveProject: "https://calculator-tewst.vercel.app/",
+  },
+  {
+    id: 8,
+    image: expense,
+    title: "Expense Tracker",
+    description:
+      "A React + TailwindCSS expense tracker. It uses Context APIto manage state",
+    sourceCode: "https://github.com/geoffreykithuku/expense-tracker",
+    liveProject: "https://expense-tracker-tewst.vercel.app/",
   },
 ];
 
