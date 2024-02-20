2d293f4fa19cf9f9573f9a9681e0838ab4237a5d
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-20 17:48:51 +0300

    add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,12 +6,16 @@ import shortener from "./url-shortener.png";
 import trendy from "./trendy.png";
 import calculator from "./calculator.png";
 import expense from "./expense.png";
+import jokester from './jokester.png';
+import rails_blog from "./rails_blog.png";
+import furnishco from "./furnishco.png";
+import netlify from './netlify.png'
 const projectsData = [
   {
     id: 1,
     image: express,
     title: "ExpressWrite",
-    description: "A blog platform using React, NodeJs, Express and MongoDB",
+    description: "A blog platform using React, NodeJs, Express and MongoDB.",
     sourceCode: "https://github.com/geoffreykithuku/ExpressWrite",
     liveProject: "https://express-write-gamma.vercel.app/",
   },
@@ -26,21 +30,20 @@ const projectsData = [
   //   liveProject:
   //     "https://phase-5-project-mental-health-app-frontend.vercel.app/",
   // },
-  // {
-  //   id: 3,
-  //   image: altcoina,
-  //   title: "AltCoina",
-  //   description:
-  //     "A realtime crypto tracking website. It uses GeckoAPI and React",
-  //   sourceCode: "https://github.com/geoffreykithuku/altcoina-crypto-website",
-  //   liveProject:
-  //     "https://633fe081f939ac0008b8cec6--resonant-croissant-165093.netlify.app/",
-  // },
+  {
+    id: 3,
+    image: altcoina,
+    title: "AltCoina",
+    description:
+      "A realtime crypto tracking website. It uses GeckoAPI and React",
+    sourceCode: "https://github.com/geoffreykithuku/altcoina-crypto-website",
+    liveProject: "https://main--altcoina.netlify.app/",
+  },
   {
     id: 4,
     image: intoxica,
     title: "Intoxica",
-    description: "Cocktail recipe lookup app using html, css and JavaScript",
+    description: "Cocktail recipe lookup app using html, css and JavaScript.",
     sourceCode: "https://github.com/geoffreykithuku/intoxica",
     liveProject: "https://geoffreykithuku.github.io/intoxica/",
   },
@@ -48,7 +51,8 @@ const projectsData = [
     id: 5,
     image: shortener,
     title: "Url Shortener",
-    description: "A url shortener built using React, TailwindCSS and Tinurl API",
+    description:
+      "A url shortener built using React, TailwindCSS and Tinurl API.",
     sourceCode: "https://github.com/geoffreykithuku/url-shortener",
     liveProject: "https://www.shorten-url.co",
   },
@@ -57,7 +61,7 @@ const projectsData = [
     image: trendy,
     title: "Trendy",
     description:
-      "A React + TailwindCSS shopping cart. It uses Redux Toolkit to manage state",
+      "A React + TailwindCSS shopping cart. It uses Redux Toolkit to manage state.",
     sourceCode: "https://github.com/geoffreykithuku/redux-toolkit-shop",
     liveProject: "https://trendy-tau.vercel.app/",
   },
@@ -66,7 +70,7 @@ const projectsData = [
     image: calculator,
     title: "Calculator",
     description:
-      "A React + TailwindCSS calculator. It uses Context API to manage state",
+      "A React + TailwindCSS calculator. It uses Context API to manage state.",
     sourceCode: "https://github.com/geoffreykithuku/react-calculator",
     liveProject: "https://calculator-tewst.vercel.app/",
   },
@@ -75,9 +79,45 @@ const projectsData = [
     image: expense,
     title: "Expense Tracker",
     description:
-      "A React + TailwindCSS expense tracker. It uses Context APIto manage state",
+      "A React + TailwindCSS expense tracker. It uses Context APIto manage state.",
     sourceCode: "https://github.com/geoffreykithuku/expense-tracker",
     liveProject: "https://expense-tracker-tewst.vercel.app/",
+  },
+  {
+    id: 9,
+    image: jokester,
+    title: "Jokester",
+    description:
+      "A React + TailwindCSS app that consumes chucknoris api and displays random jokes.",
+    sourceCode: "https://github.com/geoffreykithuku/jokester",
+    liveProject: "https://jokester.vercel.app/",
+  },
+  {
+    id: 10,
+    image: rails_blog,
+    title: "CTD Rails",
+    description:
+      "A Ruby On Rails blog application. Uses devise for auth, aws for image storage.",
+    sourceCode: "https://github.com/geoffreykithuku/rails_blog",
+    liveProject: "https://blog-5te4.onrender.com/",
+  },
+  {
+    id: 10,
+    image: furnishco,
+    title: "Furnishco",
+    description:
+      "Landing page for a furniture store. Built using html, css and bootstrap.",
+    sourceCode: "https://github.com/geoffreykithuku/rails_blog",
+    liveProject: "https://expense-tracker-tewst.vercel.app/",
+  },
+  {
+    id: 11,
+    image: netlify,
+    title: "Netlify Landing Page",
+    description:
+      "A clone of the netlify landing page. ReactJs and TailwindCSS.",
+    sourceCode: "https://github.com/geoffreykithuku/netlify-clone",
+    liveProject: "https://netlify-clone.vercel.app/",
   },
 ];
 
